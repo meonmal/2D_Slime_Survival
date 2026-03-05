e6a572169e19aa0eab3372e3c801dfff0eef6ad0
meonmal <jongmin1034@naver.com>
2026-03-05 22:28:11 +0900

    런타임 스탯을 이용한 플레이어 움직임 완성

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// 스탯의 원본 데이터
+    /// </summary>
+    [SerializeField]
+    private PlayerStatSO data;
+
+    /// <summary>
+    /// 스탯의 값을 가져오기 위한 변수
+    /// </summary>
+    private PlayerStatRuntime runtime;
+
+
+    private void Awake()
     {
-        
+        // 스탯 가져오기
+        runtime = new PlayerStatRuntime(data);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 사용할 스탯의 값을 가져온다.
+    /// </summary>
+    /// <param name="type">가져올 값의 타입</param>
+    /// <returns></returns>
+    public float GetStat(StatType type)
     {
-        
+        return runtime.GetStat(type);
+    }
+
+    /// <summary>
+    /// 스탯을 레벨업하는 함수
+    /// </summary>
+    /// <param name="type">레벨업 할 스탯의 타입</param>
+    public void LevelUp(StatType type)
+    {
+        runtime.LevelUp(type);
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,15 +2,54 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// 플레이어의 이동속도
+    /// </summary>
+    private float moveSpeed;
+
+    /// <summary>
+    /// MoveSpeed의 값을 가져오기 위한 컴포넌트
+    /// </summary>
+    private Player player;
+    /// <summary>
+    /// 플레이어의 리지드바디2D
+    /// </summary>
+    private Rigidbody2D rigid;
+
+    private void Awake()
     {
-        
+        // 컴포넌트 가져오기
+        player = GetComponent<Player>();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        /// 왜 이 작업을 Awake가 아닌 Start에서 하느냐?
+        /// Awake에서 실행한 결과 GetStat이 null이 터짐
+        /// Player에서 런타임을 가져오는 작업도 Awake에서 하기 때문
+        /// 때문에 Awake보다 한 템포 늦게 실행되는 Start에서 실행
+        moveSpeed = player.GetStat(StatType.MoveSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    /// <summary>
+    /// Rigidbody2D를 이용해서 플레이어를 이동시키는 함수
+    /// </summary>
+    private void Movement()
+    {
+        // 입력값 받아오기
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // 받아온 입력값을 정규화해서 넣어주기
+        Vector2 moveDir = new Vector2(horizontal, vertical).normalized;
+
+        // 위에서 받은 값과 Rigidbody2D를 이용해 실제로 움직이게 만든다.
+        rigid.linearVelocity = moveDir * moveSpeed;
     }
 }
